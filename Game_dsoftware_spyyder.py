8122d1e16fa1a68a33e34b5fc2ccb03bc353d1fb
Doradalberto <49616295+Doradalberto@users.noreply.github.com>
2019-04-16 10:55:54 -0300

    sem o script

--- Game_dsoftware_spyyder.py
+++ Game_dsoftware_spyyder.py
@@ -1,35 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Sun Apr 14 11:31:34 2019
-
-@author: dorad
-"""
-
-'''
-SCRIPT DA HISTORIA
-    
-Inicio: perguntar nome do jogador e da dupla
-1. Cena1: Bixo entra no insper achando que vai ser fácil > Pós semanas de PI, tem 4 dias pra fazer
- o EP com a dupla
-    Escolha: Biblioteca ou 4° Jogar Mario Kart
-        Biblioteca: Encontra amigos da sala que querem ir jogar mario kart, vão para o 4°
-        Mario Kart: Joga até tarde
-2. Cena 2: Dia seguinte, aulas de NatDes e InstruMed
-    Escolha: ir no Sujinhus pos aula ou ir fazer o EP
-        Sujinhus: Veterano valentão aparece! 
-            >> Combate! valor do dado
-            import random p/ o valor dele
-        Fazer o EP: Nao consegue entender muito bem o que e pra fazer
-3.  
-
-4. Cena 4: desespero bate, pergunta pra dupla se ele fez a parte dele e ele diz que não (fazer 
-situaçãozinha da pergunta > conversa)
-    Escolha: Ir atras do professor ou pedir ajuda pros amigos pra fazer o projeto
-        Professor: encontrar ele, 3 tentativas: import random o local que ele vai estar do insper, se vc
-        acertar, ele te da uma chance, se vc errar, vc perde
-        Amigos: nenhum dos seus amigos sabem fazer
-5. Cena 5: dia da entrega, vc nao conseguiu acabar o seu programa!
-'''
 
 hit_points = []
 dinheiro = []
